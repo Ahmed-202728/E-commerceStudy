d3fedc6be779b013a3d744df2614790465a984ba
Ahmed <ahmed-316windowslive.com>
2026-08-29 15:26:04 +0300

    Add Update on Category and Product

--- Domain/Entites/Category.cs
+++ Domain/Entites/Category.cs
@@ -7,6 +7,7 @@
         public Guid CompanyId { get; set; }
         public bool IsActive { get; set; } = true;
         public Company Company { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 
 }
--- Domain/Entites/Product.cs
+++ Domain/Entites/Product.cs
@@ -7,7 +7,9 @@
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
         public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
         public bool IsActive { get; set; } = true;
+        
     }
 
 }
